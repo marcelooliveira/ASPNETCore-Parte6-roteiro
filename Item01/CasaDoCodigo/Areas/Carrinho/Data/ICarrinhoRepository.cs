e9264639ed40c1b4fc7fd6d506bead0d9fe538ea
marcelooliveira <mclricardo@gmail.com>
2019-09-06 16:14:06 -0300

    (ainda não está ok...)

---
+++ Item01/CasaDoCodigo/Areas/Carrinho/Data/ICarrinhoRepository.cs
@@ -0,0 +1,16 @@
+﻿using MVC.Areas.Carrinho.Model;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CasaDoCodigo.Areas.Carrinho.Data
+{
+    public interface ICarrinhoRepository
+    {
+        Task<CarrinhoCliente> GetCarrinhoAsync(string clienteId);
+        IEnumerable<string> GetUsuarios();
+        Task<CarrinhoCliente> UpdateCarrinhoAsync(CarrinhoCliente carrinho);
+        Task<CarrinhoCliente> AddItemCarrinhoAsync(string clienteId, ItemCarrinho item);
+        Task<UpdateQuantidadeOutput> UpdateItemCarrinhoAsync(string clienteId, UpdateQuantidadeInput item);
+        Task<bool> DeleteCarrinhoAsync(string id);
+    }
+}
---
+++ Item01/CasaDoCodigo/Areas/Carrinho/Models/UpdateQuantidadeInput.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MVC.Areas.Carrinho.Model
+{
+    public class UpdateQuantidadeInput
+    {
+        public UpdateQuantidadeInput()
+        {
+
+        }
+
+        public UpdateQuantidadeInput(string productId, int quantity)
+        {
+            ProductId = productId;
+            Quantity = quantity;
+        }
+
+        [Required]
+        public string ProductId { get; set; }
+        [Required]
+        public int Quantity { get; set; }
+    }
+}
