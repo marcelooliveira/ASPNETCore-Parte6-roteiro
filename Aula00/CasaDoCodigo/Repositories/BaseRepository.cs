870d1f2ed89780582f46276c8ea9c716c845bf26
marcelooliveira <mclricardo@gmail.com>
2020-02-18 01:24:02 -0300

    Aula 00 (incompleto)

---
+++ Aula00/CasaDoCodigo/Repositories/BaseRepository.cs
@@ -0,0 +1,17 @@
+﻿using CasaDoCodigo.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace CasaDoCodigo.Repositories
+{
+    public abstract class BaseRepository<T> where T : BaseModel
+    {
+        protected readonly IConfiguration configuration;
+        protected readonly DbSet<T> dbSet;
+
+        public BaseRepository(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+    }
+}
