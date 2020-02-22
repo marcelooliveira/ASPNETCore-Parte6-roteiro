b104fedec8a450099e802f515785b188ddcd2071
marcelooliveira <mclricardo@gmail.com>
2020-02-22 15:34:53 -0300

    Aula04 - início

---
+++ Aula04/CasaDoCodigo/Areas/Pedido/Controllers/HomeController.cs
@@ -0,0 +1,60 @@
+﻿using System.Threading.Tasks;
+using CasaDoCodigo.Areas.Carrinho.Data;
+using CasaDoCodigo.Areas.Identity.Data;
+using CasaDoCodigo.Repositories;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CasaDoCodigo.Areas.Pedido.Controllers
+{
+    [Area("Pedido")]
+    public class HomeController : Controller
+    {
+        private readonly IPedidoRepository pedidoRepository;
+        private readonly ICarrinhoRepository carrinhoRepository;
+        private readonly UserManager<AppIdentityUser> userManager;
+
+        public HomeController(IPedidoRepository pedidoRepository,
+            ICarrinhoRepository carrinhoRepository,
+            UserManager<AppIdentityUser> userManager)
+        {
+            this.pedidoRepository = pedidoRepository;
+            this.carrinhoRepository = carrinhoRepository;
+            this.userManager = userManager;
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Authorize]
+        public async Task<IActionResult> Index(CasaDoCodigo.Models.Cadastro cadastro)
+        {
+            if (ModelState.IsValid)
+            {
+                var usuario = await userManager.GetUserAsync(this.User);
+
+                usuario.Email = cadastro.Email;
+                usuario.Telefone = cadastro.Telefone;
+                usuario.Nome = cadastro.Nome;
+                usuario.Endereco = cadastro.Endereco;
+                usuario.Complemento = cadastro.Complemento;
+                usuario.Bairro = cadastro.Bairro;
+                usuario.Municipio = cadastro.Municipio;
+                usuario.UF = cadastro.UF;
+                usuario.CEP = cadastro.CEP;
+
+                await userManager.UpdateAsync(usuario);
+
+                var carrinho = await carrinhoRepository.GetCarrinhoAsync(usuario.Id);
+                var model = await pedidoRepository.FecharPedidoAsync(carrinho, cadastro);
+                await carrinhoRepository.DeleteCarrinhoAsync(usuario.Id);
+                return base.View(model);
+            }
+            return Redirect("/Cadastro");
+        }
+    }
+
+}
---
+++ Aula04/CasaDoCodigo/Startup.cs
@@ -0,0 +1,172 @@
+﻿using CasaDoCodigo.Areas.Carrinho.Data;
+using CasaDoCodigo.Areas.Catalogo.Data;
+using CasaDoCodigo.Areas.Catalogo.Data.Repositories;
+using CasaDoCodigo.Repositories;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json.Serialization;
+using Serilog;
+using StackExchange.Redis;
+using System;
+using System.IdentityModel.Tokens.Jwt;
+
+namespace CasaDoCodigo
+{
+    public class Startup
+    {
+        private readonly ILoggerFactory _loggerFactory;
+
+        public Startup(ILoggerFactory loggerFactory,
+            IConfiguration configuration)
+        {
+            _loggerFactory = loggerFactory;
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddLogging(loggingBuilder => loggingBuilder.AddSerilog(dispose: true));
+            services.AddMvc()
+                .AddJsonOptions(options => options.SerializerSettings.ContractResolver = new DefaultContractResolver());
+
+            services.AddDistributedMemoryCache();
+            services.AddSession();
+
+            ConfigurarContexto<ApplicationContext>(services, "Default");
+            ConfigurarContextoSQLite<CatalogoDbContext>(services, "Catalogo");
+
+            ConfigurarRedis(services);
+            ConfigurarDI(services);
+
+            JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Clear();
+
+            services.AddAuthentication();
+        }
+
+        private static void ConfigurarDI(IServiceCollection services)
+        {
+            services.AddTransient<IHttpContextAccessor, HttpContextAccessor>();
+            services.AddTransient<IHttpHelper, HttpHelper>();
+            services.AddTransient<IProdutoRepository, ProdutoRepository>();
+            services.AddTransient<ICarrinhoRepository, RedisCarrinhoRepository>();
+            services.AddTransient<IPedidoRepository, PedidoRepository>();
+            services.AddTransient<ICadastroRepository, CadastroRepository>();
+        }
+
+        private void ConfigurarRedis(IServiceCollection services)
+        {
+            //By connecting here we are making sure that our service
+            //cannot start until redis is ready. This might slow down startup,
+            //but given that there is a delay on resolving the ip address
+            //and then creating the connection it seems reasonable to move
+            //that cost to startup instead of having the first request pay the
+            //penalty.
+            services.AddSingleton<IConnectionMultiplexer>(sp =>
+            {
+                var configuration = ConfigurationOptions.Parse(Configuration.GetConnectionString("RedisConnectionString"), true);
+                configuration.ResolveDns = true;
+                return ConnectionMultiplexer.Connect(configuration);
+            });
+        }
+
+        private void ConfigurarContexto<T>(IServiceCollection services, string nomeConexao) where T : DbContext
+        {
+            string connectionString = Configuration.GetConnectionString(nomeConexao);
+
+            services.AddDbContext<T>(options =>
+                options.UseSqlServer(connectionString)
+            );
+        }
+
+        private void ConfigurarContextoSQLite<T>(IServiceCollection services, string nomeConexao) where T : DbContext
+        {
+            string connectionString = Configuration.GetConnectionString(nomeConexao);
+
+            services.AddDbContext<T>(options =>
+                options.UseSqlite(connectionString)
+            );
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env,
+            IServiceProvider serviceProvider)
+        {
+            MigrateDatabase<ApplicationContext>(app);
+            CreateDatabase<CatalogoDbContext>(app);
+
+            _loggerFactory.AddSerilog();
+
+            if (env.IsDevelopment())
+            {
+                app.UseBrowserLink();
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseStaticFiles();
+            app.UseAuthentication();
+            app.UseSession();
+            app.UseMvc(routes =>
+            {
+                routes.MapAreaRoute(
+                    name: "AreaCatalogo",
+                    areaName: "Catalogo",
+                    template: "Catalogo/{controller=Home}/{action=Index}/{pesquisa?}");
+
+                routes.MapAreaRoute(
+                    name: "AreaCarrinho",
+                    areaName: "Carrinho",
+                    template: "Carrinho/{controller=Home}/{action=Index}/{codigo?}");
+
+                routes.MapAreaRoute(
+                    name: "AreaCadastro",
+                    areaName: "Cadastro",
+                    template: "Cadastro/{controller=Home}/{action=Index}");
+
+                routes.MapAreaRoute(
+                    name: "AreaPedido",
+                    areaName: "Pedido",
+                    template: "Pedido/{controller=Home}/{action=Index}");
+
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{codigo?}");
+            });
+        }
+
+        private static void MigrateDatabase<T>(IApplicationBuilder app) where T : DbContext
+        {
+            using (var serviceScope = app.ApplicationServices
+                .GetRequiredService<IServiceScopeFactory>()
+                .CreateScope())
+            {
+                using (var context = serviceScope.ServiceProvider.GetService<T>())
+                {
+                    context.Database.Migrate();
+                }
+            }
+        }
+
+        private static void CreateDatabase<T>(IApplicationBuilder app) where T : DbContext
+        {
+            using (var serviceScope = app.ApplicationServices
+                .GetRequiredService<IServiceScopeFactory>()
+                .CreateScope())
+            {
+                using (var context = serviceScope.ServiceProvider.GetService<T>())
+                {
+                    context.Database.EnsureCreated();
+                }
+            }
+        }
+    }
+}
